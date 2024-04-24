343f1cde6e3fcb5b5b685e8a2ef4088032a21f0b
Nathan Nigussie Tolosa <nathantolosa11@gmail.com>
2024-04-24 11:51:09 +0300

    Update render.js

--- server/services/render.js
+++ server/services/render.js
@@ -1,52 +1,61 @@
 const axios = require("axios");
-var Booksdb = require("../model/model.js");
-var moment = require('moment');
+var books = require("../model/model.js");
+var moment = require("moment");
 
-exports.homeRoutes = (req, res) => {
-    //make a get request to /api/users
-    Booksdb.find()
-        .then(books => {
-            res.render("index.ejs", { books });
-        })
-        .catch(err => {
-            res.status(500).send({ message: err.message || "Error occurred while Creating a create Operation" });
-        })
-}
+exports.homeRoutes = async (req, res) => {
+  try {
+    books.find().then((books) => {
+      res.render("index.ejs", { books });
+    });
+  } catch (error) {
+    res.status(500).send({
+      message:
+        err.message || "Error occurred while Creating a create Operation",
+    });
+  }
+};
+
+//make a get request to /api/users
 
 exports.add_newBook = (req, res) => {
+  res.render("add_newBook.ejs");
+};
 
-    res.render("add_newBook.ejs");
-}
 exports.view_stock = (req, res) => {
-    Booksdb.find()
-        .then(books => {
-            res.render("view_stock.ejs", { books, moment: moment });
-        })
-        .catch(err => {
-            res.status(500).send({ message: err.message || "Error occurred while Creating a create Operation" });
-        })
-}
+  books
+    .find()
+    .then((books) => {
+      res.render("view_stock.ejs", { books, moment: moment });
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message || "Error occurred while Creating a create Operation",
+      });
+    });
+};
 
 exports.update_stock = (req, res) => {
-    axios.get("http://localhost:3000/api/books", { params: { id: req.query.id } })
-        .then(function(booksdata) {
+  books
+    .findById({ _id: req.query.id })
+    .then((books) => {
+      res.render("update_stock.ejs", { books, moment: moment });
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Product Not Found!",
+      });
+    });
+};
+// exports.update_stock=(req,res) => {
+//     console.log("query1"+req.query.id)
+//     axios.get("http://localhost:3000/api/v1/books/",{params: {id: req.query.id}}).then(function(booksdata) {
 
-            res.render("update_stock.ejs", { books: booksdata.data, moment: moment });
+//         res.render("update_stock.ejs",{books: booksdata.data,moment: moment});
 
-        })
-        .catch(err => {
-            res.send(err);
-        })
-
-}
-
-exports.sells_page = (req, res) => {
-    axios.get("http://localhost:3000/api/books", { params: { id: req.query.id } })
-
-    .then(function(booksdata) {
-            res.render("sells_page.ejs", { books: booksdata.data });
-        })
-        .catch(err => {
-            res.status(500).send({ message: err.message || "Error occurred while Creating a create Operation" });
-        })
-}
+//         })
+//         .catch(err => {
+//             res.send(err);
+//         })
+//     return;
+// }
