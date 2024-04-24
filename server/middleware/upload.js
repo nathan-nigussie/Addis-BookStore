025011ac0eae96148604ab2b80fa879e5eb8cfb5
Nathan Nigussie Tolosa <nathantolosa11@gmail.com>
2024-04-24 11:52:45 +0300

    Update upload.js

--- server/middleware/upload.js
+++ server/middleware/upload.js
@@ -1,13 +1,14 @@
-const multer = require('multer')
+const multer = require("multer");
+const { v4: uuidv4 } = require("uuid");
 
 var fileStorageEngine = multer.diskStorage({
-    destination: function(req, file, cb) {
-        cb(null, './myuploads')
-    },
-    filename: function(req, file, cb) {
-        cb(null, file.fieldname + Date.now() + " - " + file.originalname);
-    }
-})
+  destination: function (req, file, cb) {
+    cb(null, "./myuploads");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + Date.now() + " - " + file.originalname);
+  },
+});
 var upload = multer({ storage: fileStorageEngine });
 
-module.exports = upload
+module.exports = upload;
